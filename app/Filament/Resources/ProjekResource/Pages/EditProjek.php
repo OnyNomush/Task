ebc18aef6db06e1514653fbcf7d01c1c703f40ef
T470s <@athaillahdeva1109>
2025-05-08 15:57:40 +0700

    fix index

--- app/Filament/Resources/ProjekResource/Pages/EditProjek.php
+++ app/Filament/Resources/ProjekResource/Pages/EditProjek.php
@@ -16,4 +16,8 @@ class EditProjek extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string {
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
 }
